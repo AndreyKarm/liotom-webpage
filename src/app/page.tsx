225ae29c96aacdf28aec20ba37797bb056af3121
AndreyKarm <karmandreii@gmail.com>
2025-04-20 02:04:15 +0100

    cat update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.page}>
         <p className={styles.text}>Hai :3</p>
+      </div>
+      <div className={styles.page}>
+        <Image src="/cat.jpg" alt="cat" width={800} height={1000}/>
       </div>
     </div>
   );
