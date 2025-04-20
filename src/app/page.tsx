36757287c9a1a1bd3e27baea9f514cb583b67f65
AndreyKarm <karmandreii@gmail.com>
2025-04-20 01:33:14 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <div>
-      Hai :3
+    <div className={styles.container}>
+      <div className={styles.page}>
+        <p className={styles.text}>Hai :3</p>
+      </div>
     </div>
   );
 }
